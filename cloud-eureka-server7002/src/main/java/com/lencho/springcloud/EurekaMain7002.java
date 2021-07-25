752a51825961a05371a835147c18ba0a75805a06
Lencho2010 <335999737@qq.com>
2021-07-25 20:09:49 +0800

    eurekaService集群配置

---
+++ cloud-eureka-server7002/src/main/java/com/lencho/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package com.lencho.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Lencho
+ * @create 2021-07-25 19:56
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
