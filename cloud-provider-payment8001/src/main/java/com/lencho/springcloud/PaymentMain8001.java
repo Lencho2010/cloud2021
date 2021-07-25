1a189ad3d82c2f8ab5b4e8d155f5a7f6498510a8
Lencho2010 <335999737@qq.com>
2021-07-25 19:32:59 +0800

    order模块修改入住eurekaService

--- cloud-provider-payment8001/src/main/java/com/lencho/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/lencho/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.lencho.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Lencho
  * @create 2021-07-25 15:54
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
