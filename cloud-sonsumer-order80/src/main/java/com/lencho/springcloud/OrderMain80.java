c233de6eda62c948f50882a3477c3b4e223fcbe4
Lencho2010 <335999737@qq.com>
2021-07-25 19:52:09 +0800

    order模块修改入住eurekaService

--- cloud-sonsumer-order80/src/main/java/com/lencho/springcloud/OrderMain80.java
+++ cloud-sonsumer-order80/src/main/java/com/lencho/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.lencho.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Lencho
  * @create 2021-07-25 17:08
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
