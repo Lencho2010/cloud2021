054a73b16f0ff9360c7f13ad5f4b073bc4257726
Lencho2010 <335999737@qq.com>
2021-07-25 20:48:16 +0800

    负载均衡

--- cloud-sonsumer-order80/src/main/java/com/lencho/springcloud/config/ApplicationContextConfig.java
+++ cloud-sonsumer-order80/src/main/java/com/lencho/springcloud/config/ApplicationContextConfig.java
@@ -1,6 +1,8 @@
 package com.lencho.springcloud.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         log.info("初始化restTemplate");
         return new RestTemplate();
