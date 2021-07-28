204b17fee369b249014cddc6869e3120aabcbdce
Lencho <335999737@qq.com>
2021-07-28 13:06:17 +0800

    gateway模块提交

---
+++ cloud-gateway-gateway9527/src/main/java/com/lencho/springcloud/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.lencho.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Package: com.lencho.springcloud
+ * @ClassName: GateWayMain9527
+ * @Author: Lencho
+ * @CreateTime: 2021/7/28 12:35
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
