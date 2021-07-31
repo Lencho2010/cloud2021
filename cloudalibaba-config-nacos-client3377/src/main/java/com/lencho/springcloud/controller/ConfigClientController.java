951917507f6abefe7294970bed7519601a76d731
Lencho2010 <335999737@qq.com>
2021-07-31 15:37:02 +0800

    nacos作为配置中心模块代码添加

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/lencho/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.lencho.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Lencho
+ * @create 2021-07-31 15:18
+ */
+@RestController
+@RefreshScope//实现配置自动更新
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
