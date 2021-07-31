2ef0d98537d1eabfd10197137b67873e3588aad0
Lencho2010 <335999737@qq.com>
2021-07-31 09:05:58 +0800

    添加springTest测试类

---
+++ MyBatis_01_HelloWorld/src/test/java/MyBatisDemo.java
@@ -0,0 +1,29 @@
+import com.lencho.mybatis.MyBatisMain8857;
+import com.lencho.mybatis.service.TestService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Lencho
+ * @create 2021-07-31 8:56
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= MyBatisMain8857.class)
+public class MyBatisDemo {
+    @Resource
+    private TestService testService;
+
+    @Test
+    public void queryStudents(){
+        List<Map> maps = testService.queryStudents();
+        System.out.println(maps);
+    }
+
+}
